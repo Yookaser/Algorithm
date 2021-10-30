3958970fc4f1b8dc191fb20a72bc55c978ee9857
kaser <yookaser@gmail.com>
2021-10-30 21:43:29 +0900

    211030 algorithm study

---
+++ Backjoon/6_S5/bj11728.py
@@ -0,0 +1,12 @@
+# 11728. 배열 합치기
+
+
+from sys import stdin
+
+
+input = stdin.readline
+N, M = map(int, input().split())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+A.extend(B)
+print(*sorted(A))
